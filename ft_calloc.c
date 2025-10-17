4fa567968c600e024bb3b45561755eec65546a46
LuN0oo <antonin.alaphilippe1@gmail.com>
2025-10-17 18:16:07 +0200

    Add files via upload

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: analaphi <analaphi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 14:44:21 by analaphi          #+#    #+#             */
-/*   Updated: 2025/10/15 19:56:58 by analaphi         ###   ########.fr       */
+/*   Updated: 2025/10/17 18:09:33 by analaphi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_calloc(size_t count, size_t size)
 	size_t	i;
 
 	i = 0;
+	if (count != 0 && count * size / count != size)
+		return (NULL);
 	tmp = malloc(count * size);
 	if (!tmp)
 		return (NULL);
